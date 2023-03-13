86cdb5944506cae5f8574100153bbf780e9bb0b0
Stepz <nine.chaitavat@gmail.com>
2023-03-13 23:24:47 +0700

    📝add toofast

---
+++ lab04/toofast.cpp
@@ -0,0 +1,17 @@
+/*
+LANG: C++
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,m,a,b,c,s;
+    cin >> n >> m;
+    for (int i=0;i<m;i++)
+    {
+        cin >> a >> b >> c >> s;
+    }
+}
