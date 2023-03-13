da1c5f5d1975f5a943a794b7aad617dbbe3a5afe
Stepz <nine.chaitavat@gmail.com>
2023-03-13 23:38:07 +0700

    📝add carrental

---
+++ lab05/carrental.cpp
@@ -0,0 +1,22 @@
+/*
+LANG: C++
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int k,n,c,p,w,a,ans=0;
+    cin >> k >> n;
+    for (int i=0;i<k;i++)
+    {
+        cin >> c >> p >> w;
+    }
+    for (int i=0;i<n;i++)
+    {
+        cin >> a;
+    }
+    cout << ans << endl;
+}
