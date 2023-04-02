b378c461f8bd5bb749c1fa5054ab744bfa6125d4
Stepz <nine.chaitavat@gmail.com>
2023-04-02 15:47:55 +0700

    ✅finish carrental

--- lab05/carrental.cpp
+++ lab05/carrental.cpp
@@ -3,20 +3,51 @@ LANG: C++
 */
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-    int k,n,c,p,w,a,ans=0;
+    int k,n,t,count,skip=0,ans=-1;
+    vector<int> tmp;
     cin >> k >> n;
+    int c[k],p[k],w[k],a[n];
+    for (int i=0;i<k;i++)
+        cin >> c[i] >> p[i] >> w[i];
+    for (int i=0;i<n;i++)
+        cin >> a[i];
     for (int i=0;i<k;i++)
     {
-        cin >> c >> p >> w;
-    }
-    for (int i=0;i<n;i++)
-    {
-        cin >> a;
+        t = c[i];
+        count = 0;
+        for (int j=0;j<n;j++)
+        {
+            int sum=0;
+            if (a[j] > w[i])
+            {   
+                skip = 1;
+                break;
+            }
+            for (auto m : tmp)
+                sum += m;
+            if (sum + a[j] > w[i])
+            {
+                count += 1;
+                tmp.clear();
+            }
+            tmp.push_back(a[j]);
+        }
+        tmp.clear();
+        if (skip == 1)
+        {
+            skip = 0;
+            continue;
+        }
+        count += 1;
+        t += count*p[i];
+        if (t < ans or ans == -1)
+            ans = t;
     }
     cout << ans << endl;
 }
