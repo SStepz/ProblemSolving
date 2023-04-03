8aee86baf1b388d3e25813bd8c3be0b928572ade
Stepz <nine.chaitavat@gmail.com>
2023-04-04 02:44:27 +0700

    ✅finish spacesignals

---
+++ DLC/spacesignals.cpp
@@ -0,0 +1,30 @@
+/*
+LANG: C++
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string s,t;
+    int pos=0,ans=0;
+    cin >> s;
+    cin >> t;
+    for (int i=0;i<t.length();i++)
+    {
+        if (t[i] == s[pos])
+        {
+            if (pos == s.length() - 1)
+            {
+                ans += 1;
+                pos = 0;
+            }
+            else
+                pos += 1;
+        }
+    }
+    cout << ans << endl;
+}
