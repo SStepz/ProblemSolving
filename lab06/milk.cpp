eb9bc2db09a75b5c015855dc60827f9950cf3bfa
Stepz <nine.chaitavat@gmail.com>
2023-04-05 19:51:12 +0700

    ✅finish milk

---
+++ lab06/milk.cpp
@@ -0,0 +1,48 @@
+/*
+LANG: C++
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int n,q;
+int par[100001];
+
+int finds(int x)
+{
+    if (par[x] == x)
+        return x;
+    return par[x] = finds(par[x]);
+}
+
+void unions(int px, int py)
+{
+    if (px == py)
+        return;
+    par[px] = py;
+}
+
+int main()
+{
+    char c;
+    int x,y,px,py;
+    cin >> n >> q;
+    for (int i=1;i<=n;i++)
+        par[i] = i;
+    for (int i=0;i<q;i++)
+    {
+        cin >> c >> x >> y;
+        px = finds(x);
+        py = finds(y);
+        if (c == 'c')
+            unions(px,py);
+        else
+        {
+            if (px == py)
+                cout << "yes" << endl;
+            else
+                cout << "no" << endl;
+        }
+    }
+}
